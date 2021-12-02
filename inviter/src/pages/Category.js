5328341b3dee73961a4a2180b65a47be9c9d123a
geraldiors <geralmers01@gmail.com>
2021-12-02 09:44:42 +0800

    minor fixing

--- inviter/src/pages/Category.js
+++ inviter/src/pages/Category.js
@@ -47,91 +47,103 @@ const GET_BY_CAT = gql`
 
 function Category() {
   const { catId } = useParams();
-  const { data } = useQuery(GET_BY_CAT, { variables: { _eq: catId } });
+  const { data, loading } = useQuery(GET_BY_CAT, { variables: { _eq: catId } });
 
   return (
     <div>
       <Navbar />
-      <h1 style={{textAlign: "center", paddingTop: "20px"}}>Activities By Category</h1>
+      <h1 style={{ textAlign: "center", paddingTop: "20px" }}>
+        Activities By Category
+      </h1>
       <hr className="cat-border" align="center" />
-      <Container style={{ paddingTop: "10px", paddingBottom: "50px" }}>
-        <Row>
-          {data?.project_fe_activities?.map((val) => (
-            <Col xs={5} className="variant1" key={val.id}>
-              <div className="card" style={{ borderRadius: "10px" }}>
-                <div className="card-title">
-                  <h4 className="title">{val.title}</h4>
-                </div>
-                <div className="card-image">
-                  <img
-                    className="activity-image"
-                    src={val.image_url}
-                    alt="activity-img"
-                  />
-                  <div className="top-left">
-                    <HiLocationMarker /> {val.city.name}
+      {loading ? (
+        <div className="cat-loading" style={{textAlign: "center"}}>
+          <Spinner animation="grow" />
+          <Spinner animation="grow" />
+          <Spinner animation="grow" />
+        </div>
+      ) : (
+        <Container style={{ paddingTop: "10px", paddingBottom: "50px" }}>
+          <Row>
+            {data?.project_fe_activities?.map((val) => (
+              <Col xs={5} className="variant1" key={val.id}>
+                <div className="card" style={{ borderRadius: "10px" }}>
+                  <div className="card-title">
+                    <h4 className="title">{val.title}</h4>
                   </div>
-                  <div className="bottom-left">
+                  <div className="card-image">
                     <img
-                      className="host-image"
-                      src={val.user.photo_url}
-                      alt="host-img"
+                      className="activity-image"
+                      src={val.image_url}
+                      alt="activity-img"
                     />
-                    <h6 className="host-tag">host</h6>
-                    <p className="host-name">{val.user.full_name}</p>
-                  </div>
-                  <div className="bottom-right">
-                    <div className="people">
-                      <BsFillPeopleFill /> {val.current_people} /{" "}
-                      {val.number_of_people}
+                    <div className="top-left">
+                      <HiLocationMarker /> {val.city.name}
                     </div>
-                    <NavLink
-                      to={`/activity/${val.id}`}
-                      className="details-link"
-                      replace
-                    >
-                      <button className="details-button">Details</button>
-                    </NavLink>
+                    <div className="bottom-left">
+                      <img
+                        className="host-image"
+                        src={val.user.photo_url}
+                        alt="host-img"
+                      />
+                      <h6 className="host-tag">host</h6>
+                      <p className="host-name">{val.user.full_name}</p>
+                    </div>
+                    <div className="bottom-right">
+                      <div className="people">
+                        <BsFillPeopleFill /> {val.current_people} /{" "}
+                        {val.number_of_people}
+                      </div>
+                      <NavLink
+                        to={`/activity/${val.id}`}
+                        className="details-link"
+                        replace
+                      >
+                        <button className="details-button">Details</button>
+                      </NavLink>
+                    </div>
+                  </div>
+                  <div className="card-stats">
+                    <Row style={{ paddingTop: "50px" }}>
+                      <Col className="stats-col">
+                        <div className="centered">
+                          <BsFillCalendar2WeekFill
+                            size={16}
+                            className="date-icon"
+                          />
+                          <span>{val.date}</span>
+                        </div>
+                      </Col>
+                      <Col className="stats-col" xs={5}>
+                        <div className="centered">
+                          <BiTimeFive size={16} className="time-icon" />
+                          <span>{val.time.slice(0, 5)}</span>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row style={{ paddingTop: "12px", textAlign: "center" }}>
+                      <Col className="stats-col">
+                        <AiFillHeart size={22} color="deeppink" />
+                        <p className="stats-text">
+                          {val.likes_aggregate.aggregate.count}
+                        </p>
+                      </Col>
+                      <Col className="stats-col">
+                        <BsFillShareFill />
+                        <p className="stats-text">share</p>
+                      </Col>
+                      <Col className="stats-col">
+                        <GrFlag />
+                        <p className="stats-text">Report</p>
+                      </Col>
+                    </Row>
                   </div>
                 </div>
-                <div className="card-stats">
-                  <Row style={{ paddingTop: "50px" }}>
-                    <Col className="stats-col">
-                      <div className="centered">
-                        <BsFillCalendar2WeekFill
-                          size={16}
-                          className="date-icon"
-                        />
-                        <span>{val.date}</span>
-                      </div>
-                    </Col>
-                    <Col className="stats-col" xs={5}>
-                      <div className="centered">
-                        <BiTimeFive size={16} className="time-icon" />
-                        <span>{val.time.slice(0, 5)}</span>
-                      </div>
-                    </Col>
-                  </Row>
-                  <Row style={{ paddingTop: "12px", textAlign: "center" }}>
-                    <Col className="stats-col">
-                      <AiFillHeart size={22} color="deeppink" />
-                      <p className="stats-text">{val.likes_aggregate.aggregate.count}</p>
-                    </Col>
-                    <Col className="stats-col">
-                      <BsFillShareFill />
-                      <p className="stats-text">share</p>
-                    </Col>
-                    <Col className="stats-col">
-                      <GrFlag />
-                      <p className="stats-text">Report</p>
-                    </Col>
-                  </Row>
-                </div>
-              </div>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      )}
     </div>
   );
 }
